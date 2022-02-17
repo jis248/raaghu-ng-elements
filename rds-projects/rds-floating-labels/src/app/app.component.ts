3191c2138f79d5c16c07d70af12db0e1b5fbca51
Niphy Anto <niphy.anto@waiin.com>
2022-02-17 18:02:51 +0530

    Created floating labels

--- rds-projects/rds-floating-labels/src/app/app.component.ts
+++ rds-projects/rds-floating-labels/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rds-floating-labels';
+  @Input() label: string = 'Floating Label';
+  @Input() id: string = 'floatingInput';
+  @Input() name: string = 'floatingInputName';
+  @Input() value: string;
+  @Input() required: boolean = false;
+  @Input() disabled: boolean = false;
+  @Input() multiple: boolean = false;
+  @Input() placeholder: string = '';
+  @Input() type: 'email' | 'text' | 'number' | 'password' | 'textarea' | 'selection' = 'text';
+  @Input() listItems = ['one', 'two', 'three'];
+  onSelect(event:any){
+  
+  }
 }
