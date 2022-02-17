051ad6a33ef038179453aa23621e5432475edbdb
Ketan Khalate <ketan.khalate@waiin.com>
2022-02-17 15:36:52 +0530

    Added table element

---
+++ rds-projects/rds-table/src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input, OnInit, TemplateRef, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+})
+export class AppComponent implements OnInit {
+  title = 'rds-table';
+
+  @Input()
+  hover = false
+
+  @Input()
+  tableItems: any = [{ name: "xyz", age: 25, state: "Maharashtra" }, { name: "yz", age: 21, state: "Hariyana" }]
+
+  @Input()
+  thead!: TemplateRef<any>;
+
+  @Input()
+  tbody!: TemplateRef<any>
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+  ngAfterViewInit() {
+
+  }
+
+  public get class(): string {
+    var hover = ''
+    if (this.hover === true) {
+
+      hover = 'table-hover'
+    }
+    return hover
+  }
+}
