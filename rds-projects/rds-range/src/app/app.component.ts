dce1b0968fe81f0763845fb9fe23d42df5d07c38
Niphy Anto <niphy.anto@waiin.com>
2022-02-18 23:45:22 +0530

    stories added for rds-range element

--- rds-projects/rds-range/src/app/app.component.ts
+++ rds-projects/rds-range/src/app/app.component.ts
@@ -10,17 +10,12 @@ export class AppComponent implements OnInit, ControlValueAccessor {
   onChange!: (value: string) => void;
   onTouched!: () => void;
 
-  vale?: number
-
-
-  @Input()
-  customstyle: "style1" | "style2" | "style3" = "style1"
-  @Input()
-  max = 1000
-
-  @Input()
-  min = 500
-
+  @Input() customstyle: "style1" | "style2" | "style3" = "style1"
+  @Input() max = 1000;
+  @Input() min = 500;
+  vale?: number;
+  // @Input() step = "0.5";
+  @Input() disabled: boolean = false;
   rangeval!: HTMLInputElement
 
   writeValue(obj: any): void {
@@ -51,8 +46,8 @@ export class AppComponent implements OnInit, ControlValueAccessor {
     let tooltip = document.getElementById('tooltip') as HTMLSpanElement
     let length = this.max - this.min
     let progress = ((parseInt(this.rangeval.value) - this.min) / length) * 100
-    this.rangeval.style.backgroundImage = `linear-gradient(90deg,#5C82E3 ${progress}%,#D0D7DD 0%)`
-    //progress = progress - 0.95
+    // this.rangeval.style.backgroundImage = `linear-gradient(90deg,#5C82E3 ${progress}%,#D0D7DD 0%)`
+    progress = progress - 0.95
     tooltip.style.left = `calc(${progress}% + (${-6 - progress * 0.15}px))`
     this.currentValue.emit(this.vale)
   }
