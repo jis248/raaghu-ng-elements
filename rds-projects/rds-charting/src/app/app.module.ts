4ad8381e0a81c35649b70d17bbf09a39f4b2350a
Shrikant Birajdar <shrikant.birajdar@waiin.com>
2022-02-22 16:48:35 +0530

    Added rds-charting element as a blank element structure.

---
+++ rds-projects/rds-charting/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { ApplicationRef, DoBootstrap, Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { BrowserModule } from '@angular/platform-browser';
+import { environment } from 'rds-projects/rds-datepicker/src/environments/environment.prod';
+
+import { AppRoutingModule } from './app-routing.module';
+import { RdsChartingComponent } from './rdsCharting.component';
+
+@NgModule({
+  declarations: [
+    RdsChartingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  entryComponents: [RdsChartingComponent],
+  bootstrap: [environment.localDev ? RdsChartingComponent : []],
+})
+export class AppModule implements DoBootstrap {
+  constructor(private injector: Injector) {
+    const micro = createCustomElement(RdsChartingComponent, {
+      injector: this.injector,
+    });
+    customElements.define('rds-charting', micro);
+  }
+
+  ngDoBootstrap(appRef: ApplicationRef): void {
+  }
+}
---
+++ rds-projects/rds-charting/src/app/rds-charting.stories.ts
@@ -0,0 +1,13 @@
+import { Story, Meta } from '@storybook/angular/types-6-0';
+import { RdsChartingComponent } from './rdsCharting.component';
+
+
+export default {
+    title: 'ELEMENTS/rds-charting',
+    component:RdsChartingComponent ,
+} as Meta
+
+const Template: Story<RdsChartingComponent> = (args:RdsChartingComponent) => ({
+  });
+
+  export const Charting = Template.bind({})
