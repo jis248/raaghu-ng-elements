7b98c93e50e01f44a6be5c08fd3ca4db8043f411
Apoorva Lele <apoorva.lele@waiin.com>
2022-02-22 18:28:30 +0530

    Added rds-form element

---
+++ rds-projects/rds-form/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { ApplicationRef, DoBootstrap, Injector, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { environment } from 'rds-projects/rds-button/src/environments/environment';
+import { createCustomElement } from '@angular/elements';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  entryComponents: [AppComponent],
+  bootstrap: [environment.localDev ? AppComponent : []],
+})
+export class AppModule implements DoBootstrap {
+  constructor(private injector: Injector) {
+    const micro = createCustomElement(AppComponent, {
+      injector: this.injector,
+    });
+    customElements.define('rds-form', micro);
+  }
+
+  ngDoBootstrap(appRef: ApplicationRef): void {}
+}
