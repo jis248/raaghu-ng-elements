80e3de4b9390cf8cf9e68cc37fe59ea3b9f6f0a7
Neeraj Bhakre <neeraj.bhakre@waiin.com>
2022-02-23 11:52:45 +0530

    Breadcrumb added

---
+++ rds-projects/rds-breadcrumb/src/app/app.component.ts
@@ -0,0 +1,53 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { BreadcrumbsItems } from './breadcrumb-item';
+import { BreadcrumbsService } from './breadcrumbsItems.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'rds-breadcrumb';
+
+  constructor(private breadcrumbsService: BreadcrumbsService) {}
+
+   ngOnInit(): void {
+
+   this.subscription = this.breadcrumbsService.getItem().subscribe(items => {
+
+    if(items) {
+      this.breadcrumbsitems = this.customBreadcrumbsItems
+    } else {
+      this.breadcrumbsitems = [];
+    }
+  });
+  this.breadcrumbsService.sendItems(this.customBreadcrumbsItems);
+  }
+
+  subscription!: Subscription;
+  breadcrumbsitems: BreadcrumbsItems[] = [];
+
+
+  @Input()
+  customBreadcrumbsItems: BreadcrumbsItems[] = [{name:'Home',route:'/home',iconClass: 'bi bi-house-door'},{name:'About',route:'/About',iconClass: 'bi bi-house-door'},{name:'Contact US',route:'/Contact',iconClass: 'bi bi-house-door'},{name:'Services',route:'/Services',iconClass: 'bi bi-house-door'},{name:'Contact US',route:'/Contact',iconClass: 'bi bi-house-door'}];
+  
+  @Input() color ?: string;
+  @Input() role : 'basic' | 'Advanced' | 'withDivider'=  'basic';
+  @Input() iconShow : boolean = false;
+
+ @Output() onNavigation = new EventEmitter<Event>();
+
+ activepage?: number;
+ onClick(item:any,i:number): void {
+
+ this.activepage=i;
+ this.onItemClick.emit(item);
+
+}
+
+  @Output()
+  onItemClick = new EventEmitter<any>();
+
+}
