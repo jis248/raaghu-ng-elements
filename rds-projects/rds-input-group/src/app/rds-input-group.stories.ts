cac92d09d591104434314650edcce329f16f054d
Niphy Anto <niphy.anto@waiin.com>
2022-02-25 11:36:06 +0530

    enhancement in rds-input-group element

--- rds-projects/rds-input-group/src/app/rds-input-group.stories.ts
+++ rds-projects/rds-input-group/src/app/rds-input-group.stories.ts
@@ -1,23 +1,18 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { moduleMetadata } from '@storybook/angular';
+
 import { Story, Meta } from '@storybook/angular/types-6-0';
 import { AppComponent as RdsInputGroupComponent } from './app.component';
-import { AppComponent as RdsInputComponent } from '../../../rds-input/src/app/app.component';
-import { AppComponent as RdsSelectListComponent } from '../../../rds-select-list/src/app/app.component';
+
 
 export default {
 
     title: 'Elements/Input Group',
     component: RdsInputGroupComponent,
-    decorators: [
-        moduleMetadata({
-            declarations: [RdsInputComponent,RdsSelectListComponent],
-            schemas: [CUSTOM_ELEMENTS_SCHEMA],
-            // providers: [
-            //     { provide: APP_BASE_HREF, useValue: "/" }
-            // ]
-        }),
-    ],
+    // decorators: [
+    //     moduleMetadata({
+    //         declarations: [RdsInputComponent, RdsSelectListComponent],
+    //         schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    //     }),
+    // ],
     argTypes: {
         backgroundColor: { control: 'color' },
     },
@@ -26,25 +21,9 @@ export default {
 export const inputGroup: Story<RdsInputGroupComponent> = (args) => ({
     props: args,
 
-    template: `<rds-input-group 
-  [inputGroupItems]="inputGroupItems">
-    <ng-template #inputGroupItems>
-    <rds-input [inputType]="'text'"></rds-input>
-    <rds-input [inputType]="'file'"></rds-input>
-    <rds-select-list
-    size="default"
-    selectSize="default"
-    [showSizeAttribute]="false"
-    [disabled]="false"
-    [multiple]="false"
-    customIcon=""
-    [lisItems]="listItems"
-    [DropdownItems]="DropdownItems">
-        <ng-template #DropdownItems  let-val>
-        <option >{{val.value}}</option>
-        </ng-template>
-  </rds-select-list>
-    </ng-template>    
+    template: `<rds-input-group>
+    <input   type="text" class="form-control"  id="Errormessage" placeholder="placeholder">    
+    <button class="btn btn-outline-secondary" type="button" id="button-addon1">Button</button>  
     </rds-input-group>`
 
 });
