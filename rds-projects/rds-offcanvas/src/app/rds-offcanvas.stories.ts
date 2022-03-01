d974e4fefeab972e8f0f1366a318a2bdddf9333c
Shrikant Birajdar <shrikant.birajdar@waiin.com>
2022-03-01 15:40:13 +0530

    resolved imports error

--- rds-projects/rds-offcanvas/src/app/rds-offcanvas.stories.ts
+++ rds-projects/rds-offcanvas/src/app/rds-offcanvas.stories.ts
@@ -1,5 +1,5 @@
 import { Story, Meta } from '@storybook/angular/types-6-0';
-import { RdsOffcanvasComponent } from './rds-offcanvas.component'
+import { RdsOffcanvasComponent } from './rds-offcanvas.component';
 
 export default {
   title: 'Elements/Offcanvas',
